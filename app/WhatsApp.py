adf3da056608f2a9cdb7ffaf2122a6f1b09e87fe
joao-faust <joaovsf2004@gmail.com>
2025-01-10 13:08:29 -0300

    Add WhatsApp login
    closes #3

--- app/WhatsApp.py
+++ app/WhatsApp.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Chrome
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from qrcodeT import qrcodeT
 from time import sleep
 
 
@@ -15,13 +16,27 @@ class WhatsApp:
 
         options = Options()
         # Keeps the user session
-        # options.add_argument('--user-data-dir=./User_Data')
+        options.add_argument('--user-data-dir=./User_Data')
         # Keeps Chrome opened
-        options.add_experimental_option('detach', True)
+        # options.add_experimental_option('detach', True)
         # Runs Chrome without open it
         # options.add_argument('--headless')
 
         return Chrome(service=service, options=options)
 
     def login(self):
-        print(self.__browser)
+        self.__browser.get('https://web.whatsapp.com/')
+        # It's necessary wait because the qr code isn't imediataly rendered
+        print('Building Qr Code...')
+        sleep(5)
+        
+        qrCodeEl = self.__browser.find_element(
+            'xpath', 
+            '//*[@id="app"]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div[1]'
+        )
+        qrCodeBase64 = qrCodeEl.get_attribute('data-ref')
+        qrcodeT(qrCodeBase64)
+
+        print('You have 20 seconds to log in')
+        sleep(20)
+        self.__browser.close()
--- login.py
+++ login.py
@@ -1,5 +1,12 @@
+from selenium.common.exceptions import NoSuchElementException
+
 from app.WhatsApp import WhatsApp
 
 if __name__ == '__main__':
     whatsApp = WhatsApp()
-    whatsApp.login()
+
+    try:
+        whatsApp.login()
+    except Exception as err:
+        print('Error to log in or you are already logged in')
+        # print(err)
