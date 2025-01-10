30676cddf60319188a0db4a2b6b95a172f50868d
joao-faust <joaovsf2004@gmail.com>
2025-01-10 14:44:44 -0300

    Set up connection to Google Calendar API
    closes #5

---
+++ app/Calendar.py
@@ -0,0 +1,38 @@
+from os import path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+class Calendaer:
+
+    def __init__(self):
+        self.__scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+        self.__calendar = self.__getCalendarInstance()
+    
+    def __getCalendarInstance(self):
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file(
+                'token.json', 
+                self.__scopes
+            )
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', 
+                    self.__scopes
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        return build('calendar', 'v3', credentials=creds)
